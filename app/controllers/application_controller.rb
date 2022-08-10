2b5f04080ae5b180f2c873d5a669d8e21bd4ad1d
Jose Jerez <joseignaciojerez@gmail.com>
2022-08-10 12:30:31 -0400

    new checklist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, :except => [:index, :blank, :report, :resume]
+  before_action :authenticate_user!, :except => [:index, :blank, :report, :resume, :others]
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :masquerade_user!
 
