4d9e3204917080cae089d3e6afc1d084252f33d7
Jose Jerez <joseignaciojerez@gmail.com>
2023-12-26 00:15:02 -0300

    add graph to estado de canchas

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,6 +10,23 @@ class UserMailer < ApplicationMailer
     @greeting = "Estado de canchas " + Time.now.in_time_zone('Santiago').to_s
     @estado_cancha = EstadoCancha.last
 
+    @chart_data = {
+    'Quilimari' => @estado_cancha.quilimari,
+    'Cachinales' => @estado_cancha.cachinales,
+    'Ñilhue' => @estado_cancha.ñilhue,
+    'Caolin' => @estado_cancha.caolin,
+    'Fierro' => @estado_cancha.fierro,
+    'Puzolana' => @estado_cancha.puzolana,
+    'Tunga' => @estado_cancha.tunga,
+    'Ceniza' => @estado_cancha.ceniza,
+    'Yeso' => @estado_cancha.yeso,
+    'Conchuela' => @estado_cancha.conchuela,
+    'San Pedro' => @estado_cancha.san_pedro
+  }
+
     mail to: 'jose.jerez@msindustrial.cl, fernando.gonzalez@msindustrial.cl'
   end
+
+
+
 end
