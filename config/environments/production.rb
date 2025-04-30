c1cdd62c0eee588136dfdfb5b8af58af9ab63e30
Jose Jerez <joseignaciojerez@gmail.com>
2025-04-30 12:10:51 -0400

    smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -133,6 +133,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'melon-platform.herokuapp.com', protocol: 'https' }
 
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
 end
