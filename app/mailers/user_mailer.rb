9cf59aade965833c7fa1ac7b67f785a4b9b88cc5
Jose Ignacio Jerez Torrens <joseignaciojereztorrens@192.168.1.7>
2026-04-29 19:32:25 -0400

    new mail in camiones

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -125,7 +125,7 @@ class UserMailer < ApplicationMailer
          .average(:wait)
 
   mail(
-    to: "gaston.guerrero@meloncementos.cl, catalina.marchant@meloncementos.cl, geraldinne.martinez@meloncementos.cl, manuel.salas@meloncementos.cl, camila.ramirez@meloncementos.cl, rodolfo.tobar-externo@meloncementos.cl, karime.merino@meloncementos.cl, jose.jerez@msindustrial.cl, fernando.gonzalez@msindustrial.cl, simon.rojas@msindustrial.cl, marcos.prospero@msindustrial.cl, hernan.martinez@msindustrial.cl, alejandro.olivares@msindustrial.cl, administracion@msindustrial.cl",
+    to: "gaston.guerrero@meloncementos.cl, catalina.marchant@meloncementos.cl, geraldinne.martinez@meloncementos.cl, manuel.salas@meloncementos.cl, camila.ramirez@meloncementos.cl, rodolfo.tobar-externo@meloncementos.cl, karime.merino@meloncementos.cl, jose.jerez@msindustrial.cl, fernando.gonzalez@msindustrial.cl, simon.rojas@msindustrial.cl, marcos.prospero@msindustrial.cl, hernan.martinez@msindustrial.cl, alejandro.olivares@msindustrial.cl, administracion@msindustrial.cl, misael.garrido@meloncementos.cl",
     subject: "Reporte camiones - #{now.strftime('%d/%m/%Y %H:%M')}"
   )
 end
